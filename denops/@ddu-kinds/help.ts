736ee7d05a42d304c9beadfff250ca9aa60caa89
uga-rosa <uga6603@gmail.com>
2023-06-12 04:00:07 +0900

    Preview with bat; buffer preview is so slow

--- denops/@ddu-kinds/help.ts
+++ denops/@ddu-kinds/help.ts
@@ -3,9 +3,10 @@ import {
   ActionFlags,
   BaseKind,
   DduItem,
+  PreviewContext,
   Previewer,
-} from "https://deno.land/x/ddu_vim@v2.8.4/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v2.8.4/deps.ts";
+} from "https://deno.land/x/ddu_vim@v3.0.2/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v3.0.2/deps.ts";
 
 export type ActionData = {
   word: string;
@@ -20,7 +21,9 @@ type OpenParams = {
 type Params = Record<never, never>;
 
 export class Kind extends BaseKind<Params> {
-  actions: Record<
+  contents?: Record<string, string[]>;
+
+  override actions: Record<
     string,
     (args: ActionArguments<Params>) => Promise<ActionFlags>
   > = {
@@ -44,7 +47,7 @@ export class Kind extends BaseKind<Params> {
 
       const action = items[0]?.action as ActionData;
       await denops.cmd(`silent ${openCommand} help ${action.word}`);
-      return Promise.resolve(ActionFlags.None);
+      return ActionFlags.None;
     },
     vsplit: (args: ActionArguments<Params>) => {
       return this.actions["open"]({
@@ -60,19 +63,31 @@ export class Kind extends BaseKind<Params> {
     },
   };
 
-  getPreviewer(args: {
+  override async getPreviewer(args: {
+    denops: Denops;
     item: DduItem;
+    previewContext: PreviewContext;
   }): Promise<Previewer | undefined> {
     const action = args.item.action as ActionData;
     if (!action || !action.path) {
-      return Promise.resolve(undefined);
+      return;
     }
-    return Promise.resolve({
-      kind: "buffer",
-      path: action.path,
-      pattern: action.pattern,
-      syntax: "help",
-    });
+    const path = action.path;
+
+    if (!this.contents) {
+      this.contents = {};
+    }
+    if (!this.contents[path]) {
+      this.contents[path] = (await Deno.readTextFile(path)).split(/\r?\n/);
+    }
+    const lineNr = this.contents[path].findIndex((line) => line.includes(action.pattern)) + 1;
+
+    const [start, end] = [lineNr, lineNr + args.previewContext.height];
+
+    return {
+      kind: "terminal",
+      cmds: ["bat", "-n", path, "-r", `${start}:${end}`, "--highlight-line", "0"],
+    };
   }
 
   params(): Params {
